622edacb65eeb5df8dddb04027ed59f7ad5a1c2e
Lucas Freitas Gonçalves Ribeiro <lucasfreitasr@outlook.com>
2022-11-07 00:29:07 -0300

    add novos exercicios

--- Kotlin/src/main/kotlin/Exercicio4While.kt
+++ Kotlin/src/main/kotlin/Exercicio4While.kt
@@ -1,4 +1,5 @@
 fun main() {
+    /*
     val litrosCaixaDagua = 2000f
     var qtdBaloesDgua = 0f
     var contador = 0f
@@ -16,6 +17,8 @@ fun main() {
     println("Exericio 2")
     fizzBuzz()
 
+    println()
+
     println("Exercicio 3")
     println("Digite uma String: ")
     val string = readLine()
@@ -23,6 +26,19 @@ fun main() {
         invertTexto(string)
     }
 
+    println("Exercicio 4")
+    println("Digite uma String: ")
+    val string2 = readLine()
+    if(string2 != null){
+        searchX(string2)
+    }
+
+    println()
+    println()
+    println("Desafio")
+
+     */
+    investRetorno()
 }
 
 fun fizzBuzz(){
@@ -44,4 +60,49 @@ fun fizzBuzz(){
 fun invertTexto (text:String){
     val  lengthText = text.length
     print(text.reversed())
+}
+
+fun searchX(str:String){
+    val stringLength = str.length
+    var i = 0
+    var qtdX = 0
+    var qtdO = 0
+    while (i < stringLength){
+
+        if(str[i] == 'x'){
+            qtdX += 1
+        }else if(str[i]== 'o'){
+            qtdO += 1
+        }
+        i++
+    }
+    if(qtdO == qtdX){
+        print(true)
+    }else{
+        print(false)
+    }
+}
+fun investRetorno (){
+    var i = 0
+    val qtdEnvestida = 5 / 10 * 10000
+    var investiAna = 10000
+    var investiPaula = 5000
+    while(true){
+        val rendiMentoAna = (2 * investiAna) / 100
+        val rendiMentoPaula = (8 * investiPaula) / 100
+        investiAna += rendiMentoAna
+        investiAna += qtdEnvestida
+
+        investiPaula += rendiMentoPaula
+        
+
+        if( investiPaula > investiAna){
+            println("$i meses")
+            println("TOTAL PAULA: $investiPaula")
+            print("TOTAL ANA: $investiAna")
+            break
+        }
+        i++
+    }
+
 }
